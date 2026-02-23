b8fc76c42fbe809711e6801c93101f7dcc3ed240
Fedor Ermolin <fermolin@mtsbank.ru>
2026-02-23 20:19:34 +0300

    [Sprint 2] add ErrorHandler

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -0,0 +1,36 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.yandex.practicum.filmorate.exeption.NotFoundException;
+import ru.yandex.practicum.filmorate.exeption.ValidationException;
+import ru.yandex.practicum.filmorate.model.ErrorResponse;
+
+@RestControllerAdvice
+@Slf4j
+public class ErrorHandler {
+
+  @ExceptionHandler(NotFoundException.class)
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  public ErrorResponse handleNotFoundException(final NotFoundException e) {
+    log.error("404: {}", e.getMessage());
+    return new ErrorResponse(e.getMessage());
+  }
+
+  @ExceptionHandler(ValidationException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ErrorResponse handleValidationException(final ValidationException e) {
+    log.error("400: {}", e.getMessage());
+    return new ErrorResponse(e.getMessage());
+  }
+
+  @ExceptionHandler(Throwable.class)
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  public ErrorResponse handleThrowable(final Throwable e) {
+    log.error("500: Произошла непредвиденная ошибка", e);
+    return new ErrorResponse("Произошла непредвиденная ошибка.");
+  }
+}
